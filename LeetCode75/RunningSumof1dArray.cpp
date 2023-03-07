4d9d1380c86f3f692295e67b2ac31987c266dbd9
Ana Clara Osias <anaclarafragaosias@gmail.com>
2023-03-07 19:21:44 -0300

    trocando nome de variavel

--- LeetCode75/RunningSumof1dArray.cpp
+++ LeetCode75/RunningSumof1dArray.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
         int size = nums.size();
-        vector<int> soma(size);
-        soma[0]=nums[0];
+        vector<int> sum(size);
+        sum[0]=nums[0];
         for(int i=1;i<size;i++)
-            soma[i] = nums[i] + soma[i-1];
-        return soma;
+            sum[i] = nums[i] + sum[i-1];
+        return sum;
     }
 };
